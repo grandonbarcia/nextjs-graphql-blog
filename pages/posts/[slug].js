ba2a9857a2978b077536928accfd999e4e28da29
Brandon Garcia <grandonleebarcia@gmail.com>
2023-02-13 18:27:52 -0800

    moved post title and author above image

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -61,27 +61,28 @@ export async function getStaticProps({ params }) {
 
 export default function BlogPost({ post }) {
   return (
-    <main className={styles.blog}>
-      <img
-        className={styles.cover}
-        src={post.coverPhoto.url}
-        alt={post.title}
-      />
-      <div className={styles.title}>
-        <div className={styles.authdetails}>
-          <img src={post.author.avatar.url} alt={post.author.name} />
-          <div className={styles.authtext}>
-            <h6>By {post.author.name} </h6>
-            <h6 className={styles.date}>{post.datePublished}</h6>
+    <section>
+      <main className={styles.blog}>
+        <div className={styles.title}>
+          <h1>{post.title}</h1>
+          <div className={styles.authdetails}>
+            <img src={post.author.avatar.url} alt={post.author.name} />
+            <div className={styles.authtext}>
+              <h6>By {post.author.name} </h6>
+              <h6 className={styles.date}>{post.datePublished}</h6>
+            </div>
           </div>
         </div>
-        <h2>{post.title}</h2>
-      </div>
-
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{ __html: post.content.html }}
-      ></div>
-    </main>
+        <img
+          className={styles.cover}
+          src={post.coverPhoto.url}
+          alt={post.title}
+        />
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.content.html }}
+        ></div>
+      </main>
+    </section>
   );
 }
